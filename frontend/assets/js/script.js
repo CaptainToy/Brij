5a89a1a18da5d424ac03a2ebcaa162cfc6873662
CaptainT0Y <abdulsalamkorede64@gmail.com>
2024-05-27 17:47:01 +0100

    last committed

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,13 +1,14 @@
-    document.addEventListener('DOMContentLoaded', function() {
-        const loginButton = document.getElementById('loginButton');
-        loginButton.addEventListener('click', function() {
-            const email = document.getElementById("form1Example13").value;
-            const password = document.getElementById("form1Example23").value;
+document.addEventListener('DOMContentLoaded', function () {
+    const loginButton = document.getElementById('loginButton');
+    loginButton.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent default form submission
+        const email = document.getElementById("form1Example13").value;
+        const password = document.getElementById("form1Example23").value;
 
-            if (email === "admin@gmail.com" && password === "admin123") {
-                window.location.href = "about.html";
-            } else {
-                alert("Error logging in");
-            }
-        })
-    });
+        if (email === "admin@gmail.com" && password === "admin123") {
+            window.location.href = "../admin/index.html";
+        } else {
+            alert("Error logging in");
+        }
+    })
+});
